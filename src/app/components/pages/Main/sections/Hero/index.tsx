2ac54a58c62e74d9138ce34ee4b4a0e0a8262990
danil.baranovskyi <danbaranovskyi@gmail.com>
2024-10-19 21:13:13 +0300

    implemented hero section of main page

--- src/app/components/pages/Main/sections/Hero/index.tsx
+++ src/app/components/pages/Main/sections/Hero/index.tsx
@@ -1,29 +1,46 @@
+import classNames from "classnames";
 import styles from "./styles.module.scss";
-import Image from 'next/image'
+import Image, { getImageProps } from 'next/image'
+import LocationIcon from "../../../../../../../public/icons/location.svg";
 
 const Hero = () => {
   return (
-    <section className={styles["hero"]}>
+    <section className={classNames(styles["hero"], 'hero')}>
       <div className={styles["hero-content"]}>
         <div className={styles["hero-text-wrapper"]}>
-          <h2 className={styles["hero-title"]}>Hello i am danya</h2>
-          <p className={styles["hero-sub-title"]}>I am super duper developer</p>
+          <h2 className={styles["hero-title"]}>Hi, I`m Danylo,😎</h2>
+          <p className={styles["hero-sub-title"]}>Lorem ipsume Lorem ipsume Lorem ipsume Lorem ipsume Lorem ipsume Lorem ipsume Lorem ipsume Lorem ipsume Lorem ipsume Lorem ipsume</p>
         </div>
         <div>
-          <span><span>icon</span><span>Kremenchug, Ukraine</span></span>
-          <span>Online now</span>
+          <span className={styles["location"]}>
+            <span className={styles['location-icon']}>
+              <LocationIcon />
+            </span>
+            <span>Kremenchug, Ukraine</span>
+          </span>
+          <span className={styles['online-now']}>Online now</span>
         </div>
         <div>
-          <button>Socials or Contact me modal</button>
+          <button className={styles['contact-me']}>Contact Me</button>
         </div>
       </div>
-      <div>
+      <div className={styles["hero-img-wrapper"]}>
         <Image
           src="/images/hero-photo.jpg"
+          width={470}
+          height={470}
+          alt="Picture of the author"
+          style={{ objectFit: "contain" }}
+        />
+        <div className={styles["border"]}></div>
+      </div>
+      <div className={classNames(styles["hero-img-wrapper"], styles["hero-img-wrapper--mobile"])}>
+        <Image
+          src="/images/hero-photo--mobile.png"
           width={300}
           height={300}
           alt="Picture of the author"
-          style={{objectFit: "contain"}}
+          style={{ objectFit: "contain" }}
         />
       </div>
     </section>
