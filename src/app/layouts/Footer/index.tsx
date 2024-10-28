5791a309178dc2a14fcd08b9821f0bbfd8b58429
danil.baranovskyi <danbaranovskyi@gmail.com>
2024-10-28 18:55:00 +0200

    implemented footer

--- src/app/layouts/Footer/index.tsx
+++ src/app/layouts/Footer/index.tsx
@@ -3,7 +3,6 @@ import styles from "./styles.module.scss";
 import Label from "@/app/components/shared/Label";
 import MailIcon from "@/public/icons/email.svg";
 import PhoneIcon from "@/public/icons/phone.svg";
-import CopyIcon from "@/public/icons/copy.svg";
 import InstaIcon from "@/public/icons/instagram.svg";
 import LinkedinIcon from "@/public/icons/linkedin.svg";
 import GitHubIcon from "@/public/icons/github.svg";
@@ -12,19 +11,23 @@ const Footer = ({ }) => {
   return (
     <footer className={classNames(styles["footer"], "footer")}>
       <div className={styles["footer-container"]}>
-        <div>
-          <div><Label>Get in touch</Label></div>
+        <div className={styles["footer-intro"]}>
+          <span><Label>Get in touch</Label></span>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi consequatur aut omnis, tempore, ullam aliquam accusantium minima, ea corrupti fuga nostrum rerum asperiores nemo repudiandae accusamus nesciunt unde vero numquam? &copy;</p>
         </div>
         <div className={styles["footer-reach-me"]}>
-          <a><MailIcon />danbaranovskyi@gmail.com<CopyIcon /></a>
-          <a><PhoneIcon />+38 095 203 28 84<CopyIcon /></a>
+          <a><MailIcon /><span>danbaranovskyi@gmail.com</span>
+          </a>
+          <a><PhoneIcon /><span>+38 095 203 28 84</span>
+          </a>
         </div>
         <div className={styles["footer-socials"]}>
-          <p>You may also find me on this platforms</p>
-          <a><InstaIcon /></a>
-          <a><LinkedinIcon /></a>
-          <a><GitHubIcon /></a>
+          <p className={styles["footer-socials-text"]}>You may also find me on this platforms</p>
+          <ul className={styles["footer-socials-list"]}>
+            <li><a><InstaIcon /></a></li>
+            <li><a><LinkedinIcon /></a></li>
+            <li><a><GitHubIcon /></a></li>
+          </ul>
         </div>
       </div>
       <div className={styles["footer-copyright"]}>
